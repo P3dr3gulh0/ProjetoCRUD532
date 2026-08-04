ed946eae4321f7db5f319c6c7f169e4b799c0410
P3dr3gulh0 <rokstondamasio20@gmail.com>
2026-08-03 21:56:04 -0300

    Esqueleto das coisa tudo

---
+++ src/model/UsuarioDAO.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.util.Date;
+
+
+public class UsuarioDAO {
+    /*	idUsuario int auto_increment primary key,
+    nome varchar(200) NOT NULL,
+    cpf varchar(14) NOT NULL UNIQUE,
+    email varchar(150) NOT NULL,
+    telefone varchar(150) NOT NULL,
+    idade int NOT NULL,
+    nascimento date NOT NULL*/
+    
+    
+    private int idUsuario;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private int idade;
+    private Date nascimento;
+    
+    
+}
